23c0f774d7d5537d700afbcb5e8e10ccf2610376
flower-emory <emory.blackwood@gmail.com>
2024-09-17 12:23:16 +0000

    bob

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ st.title("🎈hello Emory and welcome to your project🙋‍♀️(well app) ")
 st.write(
     
 )
-st.audio('peach.mp3', autoplay=True)
-
-
+#st.audio('peach.mp3', autoplay=True)
+st.video('peach.mp4', autoplay=True)
+st.image('https://static.wikia.nocookie.net/illumination/images/7/7e/Felonious_Gru_and_Kevin%2C_Stuart_%26_Bob.png/revision/latest/scale-to-width-down/250?cb=20230405205011')
 
 
 
