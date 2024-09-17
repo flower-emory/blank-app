f7eb96c89b1ab5a947be9512df61bba8ba4f6095
flower-emory <emory.blackwood@gmail.com>
2024-09-17 12:31:40 +0000

    bob caven stuert and gru standing together in nigu muves orawmd gru.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ st.write(
 )
 #st.audio('peach.mp3', autoplay=True)
 st.video('peach.mp4', autoplay=True)
-st.image('https://static.wikia.nocookie.net/illumination/images/7/7e/Felonious_Gru_and_Kevin%2C_Stuart_%26_Bob.png/revision/latest/scale-to-width-down/250?cb=20230405205011')
 
+st.image('gru.webp', width=89)
 
 
 
