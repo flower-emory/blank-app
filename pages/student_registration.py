33dc7c1b9cf6c99f0944440baa346c9e8a0d00f8
flower-emory <emory.blackwood@gmail.com>
2024-09-28 19:55:16 +0000

    bob

--- pages/student_registration.py
+++ pages/student_registration.py
@@ -4,8 +4,8 @@ import datetime
 st.title("🐚 Welcome to Zergei Junior High!")
 
 """
-We are so excited to have you joining us this year! Before you can be officially enrolled, 
-we need to collect some information from you:
+We are so not excited to have you joining us this year before you can be officially enrolled, 
+we need to collect some boring information from you:
 * Your full name
 * Your age
 * Your date of birth
@@ -13,7 +13,7 @@ we need to collect some information from you:
 * Which sports you have played so far
 * Your favorite color
 * Have you been on your official school visit yet?
-* 
+* then click the submit button when you are done
 """
 
 # EXAMPLE INPUT
@@ -22,7 +22,7 @@ full_name = st.text_input("What is your full name?")
 age = st.slider("How old are you?", 1, 17,1)
 birthday = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
 picture = st.camera_input("Take a picture")
-genre = st.radio(
+sports = st.radio(
     "What's the sports you have done",
     [":rainbow[Soccer]", "***nothing***", "basketball"],
 )
@@ -32,6 +32,10 @@ if visit:
     st.write("good for you!")
 color = st.color_picker("Pick A Color", "#00f900")
 st.write("The current color is", color)
+
+
+submit =st.button("submit", type="primary")
+
 
 
 
@@ -44,13 +48,15 @@ st.write("The current color is", color)
 
 
 # EXAMPLE OUTPUT
-st.subheader("Student info")
-st.write("Full name:", full_name)
-st.write("I'm ", age, "years old")
+if submit: 
+    st.subheader("Student info")
+    st.write("Full name:", full_name)
+    st.write("I'm ", age, "years old")
 
-st.write("I was born on ", birthday)
+    st.write("I was born on ", birthday)
 
-if picture:
-    st.image(picture)
-st.write("my favorite color is", color)
-st.write("visited the school?",visit)
+    if picture:
+        st.image(picture)
+    st.write("my favorite color is", color)
+    st.write("visited the school?",visit)
+    st.write("the sports you have done are ",sports)
