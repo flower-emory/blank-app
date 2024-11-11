5dd3ed59a7b2c936b754ab06cd932f87cee95cb4
flower-emory <emory.blackwood@gmail.com>
2024-11-11 22:42:52 +0000

    bob

--- pages/cats.py
+++ pages/cats.py
@@ -2,14 +2,18 @@
 import streamlit as st
 
 import requests
+def get_image(num):
+    
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{num}/")
 
+
+    st.image(response.json()['sprites']['other']["showdown"]["front_default"], use_column_width=True)
+    st.image(response.json()["sprites"]["front_default"], use_column_width=True)
 num = st.slider("", 1, 130, 25)
+get_image(num)
 
-response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{num}/")
 
-st.image(response.json()['sprites']['other']["showdown"]["front_default"], use_column_width=True)
 
-st.image(response.json()["sprites"]["front_default"], use_column_width=True)
-# st.write(pyjokes.get_joke())
-st.write("hello my name is greg")
+
+
 
