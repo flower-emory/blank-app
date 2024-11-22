6759b87257e88c2f57a1c011fcaca7d60939ea91
flower-emory <emory.blackwood@gmail.com>
2024-11-22 15:48:18 +0000

    bob

--- pages/to_do_list.py
+++ pages/to_do_list.py
@@ -4,12 +4,12 @@ import streamlit as st
 t = st.text_input("make your own to do list", value=None)
 st.write("", t)
 
-if "my_stuff" not in st.session_state:
-    st.session_state["my_stuff"] = []
+if "stuff_to_do" not in st.session_state:
+    st.session_state["stuff_to_do"] = []
 
 if st.button("Add a chex box"):
-    st.session_state["my_stuff"].append(t)
-    
+    st.session_state["stuff_to_do"].append(t)
 
-st.write("SESSION STATE:")
+
+
 st.write(str(st.session_state))
