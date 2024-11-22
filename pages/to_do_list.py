0a16d9d2775ee60fcb98f8e0dc9bcca78a431b2a
flower-emory <emory.blackwood@gmail.com>
2024-11-22 15:41:06 +0000

    bob

--- pages/to_do_list.py
+++ pages/to_do_list.py
@@ -8,10 +8,8 @@ if "my_stuff" not in st.session_state:
     st.session_state["my_stuff"] = []
 
 if st.button("Add a chex box"):
-    st.session_state["my_stuff"].append(t,agree = st.checkbox("I agree")
-
-if agree:
-    st.write("Great!") )
+    st.session_state["my_stuff"].append(t)
+    
 
 st.write("SESSION STATE:")
 st.write(str(st.session_state))
