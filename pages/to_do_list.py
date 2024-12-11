b9723c194bc4d089c2f9f8bb8711341df088607d
flower-emory <emory.blackwood@gmail.com>
2024-12-11 19:02:10 +0000

    bob

--- pages/to_do_list.py
+++ pages/to_do_list.py
@@ -1,15 +1,26 @@
-import streamlit
 import streamlit as st
+st.write("Hello☺️! This is a helpful daily schedule to help you be more: Fabulously Encouraging, Lovely, Incredible, Careing, Incomprehendibly Trustworthy, Yummilicious!") 
 
-t = st.text_input("make your own to do list", value=None)
-st.write(", t", "")
+# Part 1:
+# This puts some starting values in the list of todos
+if "TODO_LIST" not in st.session_state:
+    st.session_state["TODO_LIST"] = []
 
-if "stuff_to_do" not in st.session_state:
-    st.session_state["stuff_to_do"] = []
+# Part 2:
+# Gives you a box to write in
+# And then there's a button
+# If you push that button, it adds the thing you typed to the list
+title = st.text_input("Add your own")
+if st.button("add a thing"):
+    st.session_state["TODO_LIST"].append(title)
 
-if st.button("Add a chex box"):
-    st.session_state["stuff_to_do"].append(t)
+
+# Part 3:
+# For each item in your list, it makes a checkbox
+for checkbox in st.session_state["TODO_LIST"]:
+    st.checkbox(checkbox)
 
 
+#st.write("SESSION STATE:")
+#st.write(str(st.session_state))
 
-st.write(str(st.session_state))
